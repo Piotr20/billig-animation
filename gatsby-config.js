dd407cd5c7aa034e308357e319f4b9eccc82bc2a
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2021-05-18 16:53:13 +0200

    schema reduced

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,11 @@ module.exports = {
         url: "http://wp.piotrpospiech.com/billig-animation/graphql",
         type: {
           BlockEditorContentNode: { exclude: true },
+          schema: {
+            perPage: 20, // currently set to 100
+            requestConcurrency: 5, // currently set to 15
+            previewRequestConcurrency: 2, // currently set to 5
+          },
         },
       },
     },
