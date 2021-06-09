c0cc1d4acb7d23b156db338fcc307591d41a47f4
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2021-06-09 10:48:45 +0200

    process guide draft 3

--- src/components/productionSteps.js
+++ src/components/productionSteps.js
@@ -68,7 +68,7 @@ const ProductionSteps = () => {
                 1
               </div>
             </div>
-            <div className="py-2 md:py-4 bg-white rounded-full">
+            <div className="py-2 md:py-4 bg-white rounded-full relative line-top">
               <div className="text-2xl md:text-4xl xl:text-6xl font-semibold text-black shadow-lg rounded-full  w-10 h-10 md:w-12 md:h-12 xl:w-14 xl:h-14  bg-blue flex items-center justify-center border-white border-3 md:border-6">
                 2
               </div>
@@ -78,12 +78,12 @@ const ProductionSteps = () => {
                 3
               </div>
             </div>
-            <div className="py-2 md:py-4 md:hidden bg-white rounded-full">
+            <div className="py-2 md:py-4  bg-white rounded-full relative line-top">
               <div className="text-2xl md:text-4xl xl:text-6xl font-semibold text-black shadow-lg rounded-full  w-10 h-10 md:w-12 md:h-12 xl:w-14 xl:h-14 bg-yellow flex items-center justify-center border-white border-3 md:border-6">
                 4
               </div>
             </div>
-            <div className="py-2 md:py-4 md:hidden bg-white rounded-full">
+            <div className="py-2 md:py-4  bg-white rounded-full">
               <div className="text-2xl md:text-4xl xl:text-6xl font-semibold text-black shadow-lg rounded-full  w-10 h-10 md:w-12 md:h-12 xl:w-14 xl:h-14 bg-blue flex items-center justify-center border-white border-3 md:border-6">
                 5
               </div>
@@ -92,58 +92,23 @@ const ProductionSteps = () => {
           {boxes.map((box, key) => {
             return (
               <div
-                className={`rounded-xl md:mt-12 md:mx-4 xl:mt-16 md:rounded-2xl gsap-anim-item-steps shadow-lg p-4 md:p-6  w-4/5 my-4 md:my-0 md:w-1/3 ${
+                className={`rounded-xl  md:mt-12 md:mx-3 relative xl:mt-16 md:rounded-2xl gsap-anim-item-steps shadow-lg p-4 md:p-6  w-4/5 my-4 md:my-0 md:w-1/5 ${
                   key === 1 || key === 3
                     ? `self-end   md:self-start`
                     : `self-end md:self-start`
                 } ${key === 0 || key === 3 ? ` bg-yellow` : ``}  ${
-                  key === 1 || key === 4 ? ` xl:mx-12 bg-blue` : ``
+                  key === 1 || key === 4 ? `  bg-blue` : ``
                 } ${key === 2 || key === 5 ? ` bg-red` : ``} ${
-                  key > 2 ? `md:hidden` : ``
-                } `}
-                key={key}
-              >
-                <h3 className={` md:leading-tight`}>
-                  {box.node.blocks[0].attributes.content}
-                </h3>
-                <p className={`py-2 md:py-3  `}>
-                  {box.node.blocks[1].attributes.content}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-        <div className=" w-full hidden md:flex relative my-8 md:mt-24   flex-col md:flex-row justify-between md:justify-around">
-          <div className="absolute h-full md:w-full md:h-2 shadow-2xltramsform -translate-x-1/2 w-2 bg-black rounded-2xl md:rounded-3xl flex flex-col md:flex-row items-center justify-around">
-            <div className="py-2 md:py-4 hidden md:block bg-white rounded-full">
-              <div className="text-2xl md:text-4xl xl:text-6xl font-semibold text-black shadow-lg rounded-full  w-10 h-10 md:w-12 md:h-12 xl:w-14 xl:h-14 bg-yellow flex items-center justify-center border-white border-3 md:border-6">
-                4
-              </div>
-            </div>
-            <div className="py-2 md:py-4 hidden md:block bg-white rounded-full">
-              <div className="text-2xl md:text-4xl xl:text-6xl font-semibold text-black shadow-lg rounded-full  w-10 h-10 md:w-12 md:h-12 xl:w-14 xl:h-14 bg-blue flex items-center justify-center border-white border-3 md:border-6">
-                5
-              </div>
-            </div>
-          </div>
-          {boxes.map((box, key) => {
-            return (
-              <div
-                className={`rounded-xl md:mt-12 xl:mt-16 md:rounded-2xl gsap-anim-item-steps shadow-lg p-4 md:p-6  w-4/5 my-4 md:my-0 md:w-1/3 ${
                   key === 1 || key === 3
-                    ? `self-end   md:self-start`
-                    : `self-end md:self-start`
-                } ${key === 0 || key === 3 ? ` bg-yellow` : ``}  ${
-                  key === 1 || key === 4 ? `md:mx-6 xl:mx-12 bg-blue` : ``
-                } ${key === 2 || key === 5 ? ` bg-red` : ``} ${
-                  key < 3 ? `md:hidden` : ``
+                    ? `md:mt-20 xl:mt-24 md:relative md:top-20 `
+                    : ``
                 } `}
                 key={key}
               >
-                <h3 className={` md:leading-tight`}>
+                <h3 className={`text-center md:leading-tight`}>
                   {box.node.blocks[0].attributes.content}
                 </h3>
-                <p className={`py-2 md:py-3  `}>
+                <p className={`py-2 md:py-3 md:hidden `}>
                   {box.node.blocks[1].attributes.content}
                 </p>
               </div>
